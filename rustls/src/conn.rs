2b60f48fabd30209c6a30ae07abf621187462c87
Daniel McCarney <daniel@binaryparadox.net>
2024-01-10 12:39:50 -0500

    conn: fix stale comment on first_handshake_message
    The existing rustdoc comment references `process_handshake_message`
    - I don't think this fn ever existed (?), but a similarly named
    `process_new_handshake_messages` fn did exist for a time. It was removed
    in 1acd7a, leaving only `process_new_packets` and `process_msg`. This
    commit updates the comment accordingly.

--- rustls/src/conn.rs
+++ rustls/src/conn.rs
@@ -469,8 +469,8 @@ impl<Data> ConnectionCommon<Data> {
 
     /// Extract the first handshake message.
     ///
-    /// This is a shortcut to the `process_new_packets()` -> `process_msg()` ->
-    /// `process_handshake_messages()` path, specialized for the first handshake message.
+    /// This is a shortcut to the `process_new_packets()` -> `process_msg()` path,
+    /// specialized for the first handshake message.
     pub(crate) fn first_handshake_message(&mut self) -> Result<Option<Message<'static>>, Error> {
         let mut deframer_buffer = self.deframer_buffer.borrow();
         let res = self
